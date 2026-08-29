a4db766f76309a3893a5572809dd7151073d1aeb
laguz <damsog38@gmail.com>
2026-08-29 18:17:06 -0500

    GAteway handling iot main and workspace

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,14 +8,18 @@ import (
 type Config struct{
 	Listen		  string
 	MainService  string 
+	WorkspaceService string
+	IotService string
 }
 
 func Load() Config {
 	_ = godotenv.Load()
-	
+
 	config := Config{
 		Listen: ":3000",
 		MainService: "http://localhost:4000",
+		WorkspaceService: "http://localhost:4000",
+		IotService: "http://localhost:3000",
 	}
 
 	if env_read := os.Getenv("LISTEN") ; env_read != "" {
--- main.go
+++ main.go
@@ -11,9 +11,18 @@ func main() {
 	cfg := config.Load()
 
 	MainService, err := proxy.New(cfg.MainService)
-
 	if err != nil {
 		log.Fatalf("Main server: %v", err)
+	}
+
+	WorkspaceService, err := proxy.New(cfg.WorkspaceService)
+	if err != nil {
+		log.Fatalf("Workspace server: %v", err)
+	}
+
+	IotService, err := proxy.New(cfg.IotService)
+	if err != nil {
+		log.Fatalf("Iot server: %v", err)
 	}
 
 	mux := http.NewServeMux()
@@ -23,7 +32,18 @@ func main() {
 		_, _ = writer.Write([]byte("ok"))
 	})
 
+	// Endpoints
+	
+	mux.Handle("/workspace", WorkspaceService)
+	mux.Handle("/workspace/", WorkspaceService)
+	mux.Handle("/iot", IotService)
+	mux.Handle("/iot/", IotService)
+
 	mux.Handle("/", MainService)
+
+	log.Printf("Workspace server: %s", cfg.WorkspaceService)
+	log.Printf("Iot server: %s", cfg.IotService)
+	log.Printf("Main server: %s", cfg.MainService)
 	log.Printf("gateway listening on %s -> %s", cfg.Listen, cfg.MainService)
 
 	if err := http.ListenAndServe(cfg.Listen, mux); err != nil {
